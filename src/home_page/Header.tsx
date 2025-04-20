a9a4a282e6cf00d4dbf0798c26521f0614b425e5
Devon Wilkes <Your_Email>
2025-04-20 18:44:30 -0400

    working on establishing pages

---
+++ src/home_page/Header.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+const Header: React.FC = () => {
+    return (
+        <div className = "header">
+            <h1>WhereToWaste</h1>
+            <h2>The waste disposal search browser!</h2>
+            <ul className = "navItems">
+
+            </ul>
+
+        </div>
+    );
+ }
+
+ export default Header;
