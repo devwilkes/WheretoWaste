9b7cd8352686979c323aabb98816e420c83f28ce
Devon Wilkes <Your_Email>
2025-05-15 17:00:31 -0400

    Trying to implement site routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,6 @@ import React from "react";
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
-import Home from "./home_page/HomeBody";
 import HomeBody from "./home_page/HomeBody";
 
 // Define the App component that renders the Header, Nav, and Footer components
