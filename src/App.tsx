e4a54cba64ca3fef121845f70cce8a237c63d9cf
Devon Wilkes <Your_Email>
2025-05-15 17:14:07 -0400

    Changing IP retrieval around, adding permissions + optional location retrieval

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
-import React from "react";
+import React, {useEffect} from "react";
 import Header from "./Header";
 import Nav from "./Nav";
 import Footer from "./Footer";
 import HomeBody from "./home_page/HomeBody";
+
+// Code for a popup that appears on the first window load that asks the user for permission to use their location.
+function getUserLocation(){
+    useEffect(() => {
+    fetch('http://api.ipstack.com/172.70.110.48?access_key=4e4a77d176849dca950e8d6773f15b58')
+    .then((result) => result.json())
+    .then((resultJson) => {
+        const userIP = resultJson.ip;
+        console.log(userIP);
+        const userLocation = resultJson.location;
+        console.log(userLocation);
+
+        return userLocation;
+    })
+    }, []);
+}
 
 // Define the App component that renders the Header, Nav, and Footer components
 const App: React.FC = () => {
--- src/home_page/HomeBody.tsx
+++ src/home_page/HomeBody.tsx
@@ -1,6 +1,7 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 
-/*function getUserLocation(){
+function getUserLocation(){
+    useEffect(() => {
     fetch('http://api.ipstack.com/172.70.110.48?access_key=4e4a77d176849dca950e8d6773f15b58')
     .then((result) => result.json())
     .then((resultJson) => {
@@ -11,14 +12,15 @@ import React from 'react';
 
         return userLocation;
     })
+    }, []);
 }
     
-*/
 function search(event: React.FormEvent<HTMLFormElement>) {
     event.preventDefault();
     const formData = new FormData(event.currentTarget);
-    const userInput = formData.get("userInput");
-    console.log(userInput);
+    const userItem = formData.get("userInput");
+    const userLocation = formData.get("userInput");
+    console.log(userItem, userLocation);
 }
 
 const HomeBody: React.FC = () => {
@@ -26,7 +28,8 @@ const HomeBody: React.FC = () => {
         <div className = "SearchBody">
             <h1>What are you looking to dispose of?</h1>
             <form id = "searchForm" method = "get" onSubmit={search}>
-                <input type = "text" name = "userInput" placeholder = "Enter waste type..." required />
+                <input type = "text" name = "userItem" placeholder = "Enter waste type..." required />
+                <input type = "text" name = "userLocation" placeholder = "Enter your location..." />
                 <button type = "submit">Search</button> 
             </form>
         </div>
