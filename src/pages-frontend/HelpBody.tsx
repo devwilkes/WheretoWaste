d06bcbcf6dd574e05649bf9c41d6ee701c9810f7
Devon Wilkes <Your_Email>
2025-05-18 15:54:49 -0400

    hooking up supabase

--- src/pages-frontend/HelpBody.tsx
+++ src/pages-frontend/HelpBody.tsx
@@ -1,5 +1,32 @@
 import React from 'react';
 import './HelpStyle.css';
+import { useState, useEffect } from 'react';
+import supabase from '../server-backend/supabase';
+
+function Page() {
+  const [todos, setTodos] = useState([])
+
+  useEffect(() => {
+    function getTodos() {
+      const { data: todos } = await supabase.from('todos').select()
+
+      if (todos.length > 1) {
+        setTodos(todos)
+      }
+    }
+
+    getTodos()
+  }, [])
+
+  return (
+    <div>
+      {todos.map((todo) => (
+        <li key={todo}>{todo}</li>
+      ))}
+    </div>
+  )
+}
+
 
 async function createContactSheet() {
   await fetch(`/contacts`, {
